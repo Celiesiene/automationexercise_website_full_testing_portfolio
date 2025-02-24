ff90cb59f580b490812c80302211da5e8ed6b22f
Celiesiene <m.celiesiene@gmail.com>
2025-02-24 14:02:47 +0200

    Api Tests added

--- cypress/e2e/14ApiTesting.cy.js
+++ cypress/e2e/14ApiTesting.cy.js
@@ -33,8 +33,8 @@ describe('Api 3', () => {
 
     it("Brands have all properties", () => {
         cy.request("GET", 'https://automationexercise.com/api/brandsList').then((response) => {
-            expect(response.status).to.eq(200); 
-            const responseBody = JSON.parse(response.body); 
+            expect(response.status).to.eq(200);
+            const responseBody = JSON.parse(response.body);
             expect(responseBody).to.have.property("brands").that.is.an("array");
             responseBody.brands.forEach((brand) => {
                 expect(brand).to.have.property("id");
@@ -44,8 +44,8 @@ describe('Api 3', () => {
     });
 });
 
-describe('Api 4', ()=>{
-    it('PUT To All Brands List', ()=>{
+describe('Api 4', () => {
+    it('PUT To All Brands List', () => {
         cy.request('PUT', 'https://automationexercise.com/api/productsList').then((response) => {
             const responseBody = JSON.parse(response.body);
             expect(responseBody).to.have.property('responseCode', 405);
@@ -54,13 +54,13 @@ describe('Api 4', ()=>{
     })
 });
 
-describe('Api 5', ()=>{
+describe('Api 5', () => {
     it('POST To Search Product', () => {
         cy.request({
             method: "POST",
             url: "https://automationexercise.com/api/searchProduct",
             form: true, // Užtikrina, kad duomenys siunčiami kaip form data
-            body: { search_product: "top" } 
+            body: { search_product: "top" }
         }).then((response) => {
             expect(response.status).to.eq(200); // Patikrina, ar atsakymo kodas 200
             const responseBody = JSON.parse(response.body);
@@ -81,12 +81,189 @@ describe('Api 5', ()=>{
 });
 
 
-describe('Api 6', ()=>{
-    it('POST To Search Product without search product parameter', ()=>{
+describe('Api 6', () => {
+    it('POST To Search Product without search product parameter', () => {
         cy.request('POST', 'https://automationexercise.com/api/searchProduct').then((response) => {
             const responseBody = JSON.parse(response.body);
             expect(responseBody).to.have.property('responseCode', 400);
             expect(responseBody).to.have.property('message', 'Bad request, search_product parameter is missing in POST request.')
         });
     })
-})
+});
+
+describe('Api 7', () => {
+    it('POST To Verify Login with valid details', () => {
+        cy.request({
+            method: "POST",
+            url: "https://automationexercise.com/api/verifyLogin",
+            form: true, // Užtikrina, kad duomenys siunčiami kaip form data
+            body: {
+                email: "bebras@gmailll.com",
+                password: "bebriukas"
+            }
+        }).then((response) => {
+            expect(response.status).to.eq(200);
+            const responseBody = JSON.parse(response.body);
+            expect(responseBody).to.have.property('responseCode', 200);
+            expect(responseBody).to.have.property('message', 'User exists!')
+
+        });
+    });
+});
+
+describe('Api 8', () => {
+    it('POST To Verify Login without email parameter', () => {
+        cy.request({
+            method: "POST",
+            url: "https://automationexercise.com/api/verifyLogin",
+            form: true, // Užtikrina, kad duomenys siunčiami kaip form data
+            body: { password: "bebriukas" }
+        }).then((response) => {
+            expect(response.status).to.eq(200);
+            const responseBody = JSON.parse(response.body);
+            expect(responseBody).to.have.property('responseCode', 400);
+            expect(responseBody).to.have.property('message', 'Bad request, email or password parameter is missing in POST request.')
+
+        });
+    })
+});
+
+describe('Api 9', ()=>{
+    it('DELETE To Verify Login', ()=>{
+        cy.request('DELETE', 'https://automationexercise.com/api/verifyLogin').then((response) => {
+            const responseBody = JSON.parse(response.body);
+            expect(responseBody).to.have.property('responseCode', 405);
+            expect(responseBody).to.have.property('message', 'This request method is not supported.')
+        }); 
+    })
+});
+
+describe('Api 10', () => {
+    it('POST To Verify Login with invalid details', () => {
+        cy.request({
+            method: "POST",
+            url: "https://automationexercise.com/api/verifyLogin",
+            form: true, // Užtikrina, kad duomenys siunčiami kaip form data
+            body: {
+                email: "nera@nera.com",
+                password: "neratokio"
+            }
+        }).then((response) => {
+            expect(response.status).to.eq(200);
+            const responseBody = JSON.parse(response.body);
+            expect(responseBody).to.have.property('responseCode', 404);
+            expect(responseBody).to.have.property('message', 'User not found!')
+
+        });
+    });
+});
+
+describe('Api 11', ()=>{
+    it('POST To Create/Register User Account', ()=>{
+        cy.request({
+            method: "POST",
+            url: "https://automationexercise.com/api/createAccount",
+            form: true, // Užtikrina, kad duomenys siunčiami kaip form data
+            body: { name: "mama" ,
+                email: "mamytee@mama.lt",
+                password: "mamyte",
+                title: "Mrs",
+                birth_date: "08",
+                birth_month: "6",
+                birth_year: "1991",
+                firstname: "mama",
+                lastname: "mamyte",
+                company: "family",
+                address1: "love str",
+                address2: "22",
+                country: "heaven",
+                zipcode: "98765",
+                state: "9cloud",
+                city: "paradise",
+                mobile_number: "123123123"
+            }
+        }).then((response) => {
+            expect(response.status).to.eq(200); // Patikrina, ar atsakymo kodas 200
+            const responseBody = JSON.parse(response.body);
+            expect(responseBody).to.have.property('responseCode', 201);
+            expect(responseBody).to.have.property('message', 'User created!')
+        });
+    })
+})  
+
+describe('Api 13', ()=>{
+        it('PUT METHOD To Update User Account', ()=>{
+            cy.request({
+                method: "PUT",
+                url: "https://automationexercise.com/api/updateAccount",
+                form: true, // Užtikrina, kad duomenys siunčiami kaip form data
+                body: { name: "mama" ,
+                    email: "mamytee@mama.lt",
+                    password: "mamyte",
+                    title: "Mrs",
+                    birth_date: "08",
+                    birth_month: "6",
+                    birth_year: "1991",
+                    firstname: "mama",
+                    lastname: "mamyte",
+                    company: "family",
+                    address1: "love str",
+                    address2: "22",
+                    country: "heaven",
+                    zipcode: "98765",
+                    state: "9cloud",
+                    city: "paradise",
+                    mobile_number: "5441234"
+                }
+            }).then((response) => {
+                expect(response.status).to.eq(200); // Patikrina, ar atsakymo kodas 200
+                const responseBody = JSON.parse(response.body);
+                expect(responseBody).to.have.property('responseCode', 200);
+                expect(responseBody).to.have.property('message', 'User updated!')
+            });
+        })
+    });
+
+describe('Api 14', () => {
+    it('GET user account detail by email', () => {
+        cy.request({
+                        method: "GET",
+                        url: "https://automationexercise.com/api/getUserDetailByEmail",
+                        qs: { 
+                            email: "mamytee@mama.lt",
+                        }
+                    }).then((response) => {
+            const responseBody = JSON.parse(response.body);
+            expect(responseBody).to.have.property('responseCode', 200);
+            expect(responseBody).to.have.property('user').that.is.an('object');
+        });
+
+    });
+}) 
+
+
+
+
+
+
+describe('Api 12', ()=> {
+    it('DELETE METHOD To Delete User Account', ()=>{
+        cy.request({
+                        method: "DELETE",
+                        url: "https://automationexercise.com/api/deleteAccount",
+                        form: true, // Užtikrina, kad duomenys siunčiami kaip form data
+                        body: { 
+                            email: "mamytee@mama.lt",
+                            password: "mamyte",
+                        }
+                    }).then((response) => {
+                        expect(response.status).to.eq(200); 
+                        const responseBody = JSON.parse(response.body);
+                        expect(responseBody).to.have.property('responseCode', 200);
+                        expect(responseBody).to.have.property('message', 'Account deleted!')
+                    });
+    })
+}) 
+
+
+
